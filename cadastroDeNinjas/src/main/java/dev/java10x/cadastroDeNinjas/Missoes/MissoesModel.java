10f10d26700219233bc00712792099ec05d66358
Marcelo <you@example.com>
2026-06-23 20:15:09 -0300

    feat: Coloca o Lombok na classe

--- cadastroDeNinjas/src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
+++ cadastroDeNinjas/src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,15 @@ package dev.java10x.cadastroDeNinjas.Missoes;
 
 import dev.java10x.cadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,45 +21,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes") // uma missão tem vários ninjas
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nomeMissao, String dificuldade, NinjaModel ninjas) {
-        this.id = id;
-        this.nomeMissao = nomeMissao;
-        this.dificuldade = dificuldade;
-        this.ninjas = ninjas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNomeMissao() {
-        return nomeMissao;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nomeMissao = nomeMissao;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public NinjaModel getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(NinjaModel ninjas) {
-        this.ninjas = ninjas;
-    }
 }
